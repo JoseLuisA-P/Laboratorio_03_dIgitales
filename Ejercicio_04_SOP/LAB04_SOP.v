cbe523f09e225bb85719e8559fbe783ec92b563a
JoseLuisA-P <alv19392@uvg.edu.gt>
2020-07-30 15:29:25 -0600

    Terminado SOP4

---
+++ Ejercicio_04_SOP/LAB04_SOP.v
@@ -0,0 +1,53 @@
+module LAB04_SOP();
+
+// POS de la tabla01 del laboratorio 3
+
+  reg A, B, C, D;
+  wire NA, NB, NC, ND, w1, w2, w3, w4, w5, w6, w7, out;
+
+
+  not s1(NA,A); //(salida, entradas)
+  not s2(NB,B);
+  not s3(NC,C);
+  not s4(ND,D);
+  and mult1(w1,NA,NB,NC,ND);
+  and mult2(w2,NA,NB,C,ND);
+  and mult3(w3,NA,NB,C,D);
+  and mult4(w4,NA,B,C,ND);
+  and mult5(w5,NA,B,C,D);
+  and mult6(w6,A,NB,NC,ND);
+  and mult7(w7,A,NB,C,ND);
+  or sum1(out,w1,w2,w3,w4,w5,w6,w7);
+
+  initial begin
+    $display("A B C D| Y");
+    $display("----------");
+
+    $monitor("%b %b %b %b| %b", A, B, C, D, out);
+    A = 0; B = 0; C=0; D=0;
+    #1 A = 0; B = 0; C = 0; D = 0;
+    #1 A = 0; B = 0; C = 0; D = 1;
+    #1 A = 0; B = 0; C = 1; D = 0;
+    #1 A = 0; B = 0; C = 1; D = 1;
+    #1 A = 0; B = 1; C = 0; D = 0;
+    #1 A = 0; B = 1; C = 0; D = 1;
+    #1 A = 0; B = 1; C = 1; D = 0;
+    #1 A = 0; B = 1; C = 1; D = 1;
+    #1 A = 1; B = 0; C = 0; D = 0;
+    #1 A = 1; B = 0; C = 0; D = 1;
+    #1 A = 1; B = 0; C = 1; D = 0;
+    #1 A = 1; B = 0; C = 1; D = 1;
+    #1 A = 1; B = 1; C = 0; D = 0;
+    #1 A = 1; B = 1; C = 0; D = 1;
+    #1 A = 1; B = 1; C = 1; D = 0;
+    #1 A = 1; B = 1; C = 1; D = 1;
+    #1 $finish;
+  end
+
+  initial
+    begin
+      $dumpfile("LAB04_SOP_tb.vcd");
+      $dumpvars(0, LAB04_SOP);
+    end
+
+endmodule
