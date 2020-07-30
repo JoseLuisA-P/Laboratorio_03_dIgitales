09b1816f9b6201587a92e876b3d326c5c23b1471
JoseLuisA-P <alv19392@uvg.edu.gt>
2020-07-30 12:41:56 -0600

    Se creo el primer archivo para POS y SOP del ejercicio 01

---
+++ Ejercicio_01_POS/LAB01_POS.v
@@ -0,0 +1,40 @@
+module LAB01_POS();
+
+// POS de la tabla01 del laboratorio 3
+
+  reg A, B, C;
+  wire NA, NB, NC, w1, w2, w3, w4, w5, out;
+
+
+  not s1(NA,A); //(salida, entradas)
+  not s2(NB,B);
+  not s3(NC,C);
+  or sum1(w1,NA,NB,C);
+  or sum2(w2,A,B,NC);
+  or sum3(w3,A,NB,NC);
+  or sum4(w4,NA,NB,C);
+  and mult1(out,w1,w2,w3,w4);
+
+  initial begin
+    $display("A B C| Y");
+    $display("-------");
+
+    $monitor("%b %b %b | %b", A, B, C, out);
+    A = 0; B = 0; C=0;
+    #1 A = 0; B = 0; C=1;
+    #1 A = 0; B = 1; C=0;
+    #1 A = 0; B = 1; C=1;
+    #1 A = 1; B = 0; C=0;
+    #1 A = 1; B = 0; C=1;
+    #1 A = 1; B = 1; C=0;
+    #1 A = 1; B = 1; C=1;
+    #5 $finish;
+  end
+
+  initial
+    begin
+      $dumpfile("LAB01_POS_tb.vcd");
+      $dumpvars(0, LAB01_POS);
+    end
+
+endmodule
---
+++ Ejercicio_01_SOP/LAB_01_SOP.v
@@ -0,0 +1,41 @@
+module LAB_01_SOP();
+
+// SOP de la tabla01 del laboratorio 3
+
+  reg A, B, C;
+  wire NA, NB, NC, w1, w2, w3, w4, w5, out;
+
+
+  not s1(NA,A); //(salida, entradas)
+  not s2(NB,B);
+  not s3(NC,C);
+  and mult1(w1,NA,NB,NC);
+  and mult2(w2,NA,B,NC);
+  and mult3(w3,A,NB,NC);
+  and mult4(w4,A,NB,C);
+  and mult5(w5,A,B,C);
+  or sum1(out,w1,w2,w3,w4,w5);
+
+  initial begin
+    $display("A B C| Y");
+    $display("-------");
+
+    $monitor("%b %b %b | %b", A, B, C, out);
+    A = 0; B = 0; C=0;
+    #1 A = 0; B = 0; C=1;
+    #1 A = 0; B = 1; C=0;
+    #1 A = 0; B = 1; C=1;
+    #1 A = 1; B = 0; C=0;
+    #1 A = 1; B = 0; C=1;
+    #1 A = 1; B = 1; C=0;
+    #1 A = 1; B = 1; C=1;
+    #5 $finish;
+  end
+
+  initial
+    begin
+      $dumpfile("LAB_01_SOP_tb.vcd");
+      $dumpvars(0, LAB_01_SOP);
+    end
+
+endmodule
